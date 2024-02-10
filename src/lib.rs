a160db9cadd5367d57bdbc17a2d2a85c406385c9
Your Name <you@example.com>
2024-02-10 19:50:40 +0100

    Add query parameter

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use dialoguer::Select;
+use dialoguer::Input;
 use anyhow::Context;
 use zip;
 use reqwest::Client;
@@ -17,6 +18,7 @@ pub struct FoundAsset {
 pub struct PromptData {
     pub assettype: String,
     quality: String,
+    pub query: String,
 }
 
 pub fn prompt_user() -> PromptData {
@@ -31,6 +33,11 @@ pub fn prompt_user() -> PromptData {
         "Substance",
     ];
 
+    let query: String = Input::new()
+        .allow_empty(true)
+        .with_prompt("You can enter a search keyworkd or leave empty if you want to download all asset from type")
+        .interact_text()
+        .unwrap();
 
     let asset_type_selection = Select::new()
         .with_prompt("Wich type of asset do you want to download ? ")
@@ -50,17 +57,16 @@ pub fn prompt_user() -> PromptData {
             .items(&initial_quality)
             .interact().unwrap();
 
-
     };
 
 
-    let prompt: PromptData = PromptData { assettype: asset_type[asset_type_selection].to_string(), quality: initial_quality[quality_selection].to_string() };
+    let prompt: PromptData = PromptData { assettype: asset_type[asset_type_selection].to_string(), quality: initial_quality[quality_selection].to_string(), query: query.to_string() };
     return prompt;
 
 }
 
-pub async fn get_number_of_asset(client: &Client, asset_type: &String) -> Result<u32, anyhow::Error> {
-    let url = format!("https://ambientcg.com/api/v2/full_json?type={}", asset_type);
+pub async fn get_number_of_asset(client: &Client, asset_type: &String, query: &String) -> Result<u32, anyhow::Error> {
+    let url = format!("https://ambientcg.com/api/v2/full_json?type={}&q={}", asset_type, query );
 
     let response = client.get(url).send().await?;
     let text = response.text().await?;
@@ -76,12 +82,11 @@ pub async fn get_number_of_asset(client: &Client, asset_type: &String) -> Result
 
 pub async fn get_link(client: &Client, offset: i32, settings: &PromptData) -> Result<FoundAsset, anyhow::Error> {
 
-    let api_url = format!("https://ambientCG.com/api/v2/full_json?limit=1&type={}&offset={}", settings.assettype, offset);
+    let api_url = format!("https://ambientCG.com/api/v2/full_json?limit=1&type={}&offset={}&q={}", settings.assettype, offset, settings.query);
     let response = client.get(api_url).send().await?;
     let text = response.text().await?;
 
     let parsed_response: serde_json::Value = serde_json::from_str(&text)?;
-
 
     let assetid = match_parse(&parsed_response, "assetId".to_string())?;
     let datatype = match_parse(&parsed_response, "dataType".to_string())?;
@@ -105,9 +110,9 @@ pub async fn get_link(client: &Client, offset: i32, settings: &PromptData) -> Re
     };
 
 
-    //println!("Asset fetched: {:#?}", &asset.assetid);
     return Ok(asset);
 }
+
 
 fn match_parse(text: &Value, search: String) -> Result<String, anyhow::Error> {
     let result = text["foundAssets"][0][&search].as_str()
@@ -117,7 +122,6 @@ fn match_parse(text: &Value, search: String) -> Result<String, anyhow::Error> {
 
 pub async fn download_file(client: &Client, asset: &FoundAsset) -> Result<(), anyhow::Error> {
 
-
     let dl_url = format!("https://ambientCG.com/get?file={}", asset.file);
     let outpout_path = format!("./ambientCG/{}/{}/{}", asset.datatype, asset.category, asset.file);
 
@@ -126,7 +130,6 @@ pub async fn download_file(client: &Client, asset: &FoundAsset) -> Result<(), an
 
     let mut output_file = File::create(outpout_path)?;
     let _ = output_file.write_all(&res.bytes().await?);
-    //println!("{} downloaded successfully", asset.file);
     Ok(())
 
 }
@@ -137,17 +140,14 @@ fn create_folder_if_not_exists(category: &String, datatype: &String) -> Result<(
     if !fs::metadata(fp).is_ok() {
         fs::create_dir_all(fp)?;
     }
-    //println!("Folder created or already exists: {}", &folder_path);
     Ok(())
 }
 
 pub fn unzip_file(asset: &FoundAsset) -> Result<(), anyhow::Error> {
 
     let file_path = format!("./ambientCG/{}/{}/{}", asset.datatype, asset.category, asset.file);
-    
-    let metadata = fs::metadata(&file_path).unwrap();
 
-    if asset.file.ends_with(".zip") && metadata.len() > 0 {
+    if asset.file.ends_with(".zip") {
         let file = fs::File::open(&file_path)
             .with_context(|| format!("Failed to open file: {}", &file_path))?;
 
@@ -159,9 +159,7 @@ pub fn unzip_file(asset: &FoundAsset) -> Result<(), anyhow::Error> {
 
         let _ = fs::remove_file(&file_path)
             .with_context(|| format!("Failed to remove file: {}", &file_path))?;
-
     }
     Ok(())
 }
-
 
